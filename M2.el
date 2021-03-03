f02e28cfa831e015a9816cd5d25970953aa407ac
Doug Torrance <dtorrance@piedmont.edu>
2021-03-02 22:31:01 -0500

    M2-electric-tab modifications
    We now always tab electrically, even when not at the beginning of a
    line or on a blank line.  This has several advantages:
    
    * The function is now idempotent.
    * The "newline" function (when electric-indent-mode is on, the
      default) will do the correct thing, so we can deprecate
      "M2-newline-and-indent".
    * The new behavior is consistent with other common major modes.
    
    We also always delete leading whitespace and re-indent to clean up
    tabs and spaces.
    
    Note that it is still possible to insert a tab character with C-q TAB.

--- M2.el
+++ M2.el
@@ -639,17 +639,9 @@ be sent can be entered, with history."
 
 (defun M2-electric-tab ()
      (interactive)
-     (if (or (not (M2-in-front)) (M2-blank-line))
-	 (indent-to (+ (current-column) M2-indent-level))
-	 (let ((i (M2-this-line-indent-amount))
-	       (j (current-indentation)))
-	      (if (not (= i j))
-		  (progn
-		       (if (< i j)
-			    (delete-region (progn (beginning-of-line) (point))
-					   (progn (back-to-indentation) (point)))
-			    (back-to-indentation))
-		       (indent-to i))))))
+     (delete-region (progn (beginning-of-line) (point))
+		    (progn (back-to-indentation) (point)))
+     (indent-to (M2-this-line-indent-amount)))
 
 (defvar M2-demo-buffer
   (save-excursion
