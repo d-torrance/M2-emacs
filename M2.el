d71e1efc52dcbc1a3762726b34595bae765c7735
Doug Torrance <dtorrance@piedmont.edu>
2023-02-09 00:36:02 -0500

    Add M2-send-buffer-to-program

--- M2.el
+++ M2.el
@@ -490,6 +490,10 @@ be sent can be entered, with history."
 	 (M2-send-region-to-program send-to-buffer)
        (M2-send-line-to-program send-to-buffer)))
 
+(defun M2-send-buffer-to-program (send-to-buffer)
+  (interactive (M2--get-send-to-buffer))
+  (M2--send-to-program-helper send-to-buffer (point-min) (point-max)))
+
 (defun M2-set-demo-buffer()
   "Set the variable M2-demo-buffer to the current buffer, so that later,
 `M2-send-to-program' can obtain lines from this buffer."
